4298882d6e504d613b29f6fc0c0381d8ae5c09d3
Ian Burton <ib400@exeter.ac.uk>
2024-07-11 18:32:40 +0100

    parameter ranges update

--- Emulation/code/emulation_code/scenario_generator_1.py
+++ Emulation/code/emulation_code/scenario_generator_1.py
@@ -2,12 +2,19 @@
 """
 Created on Mon Oct 23 12:25:57 2023
 
-code for generating scenarios
+This script is for generating scenarios between a baseline and an ambitious scenario.
+The variables to be varied can be altered as well as the countries for which ambition is varied.
+Sources for the parameter ranges are provided in the comments.
+
+Possible developments:
+uncertainty_generator() currently has the variables hardcoded, could be made more flexible
+by passing a dictionary of variables and their ranges.
 
 @author: ib400
 """
 #%%
 
+# Load libraries
 import os
 import pandas as pd
 from openpyxl import load_workbook
@@ -18,21 +25,36 @@ from scipy.stats import poisson, binom, uniform, norm, randint, qmc
 import csv
 import copy
 import matplotlib.pyplot as plt
+import sys
+from pyDOE import lhs
 
-os.chdir('C:\\Users\\ib400\\OneDrive - University of Exeter\\Desktop\\PhD\\GitHub\\FTT_StandAlone')
+# Get the absolute path of the current script
+current_script_path = os.path.abspath(__file__)
 
-#%% ####### Permutation - use for both reg and CP
-# How will it work with the baseline?? Maybe we have enough space to just create them this time
-# Vary inputs using ambition_vary.regional_ambition and some sort of permutation
+# Get the absolute path of the root directory (assuming the root directory is 3 levels up from the current script)
+root_directory_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(current_script_path))))
 
-def ambition_generator(regions = ['E+','US', 'CN', 'IN', 'BR', 'ROW'], Nscens = 1):
+# Path to the 'support' directory
+emulation_directory_path = os.path.join(root_directory_path, 'Emulation', 'code', 'emulation_code')
+
+# Add the 'support' directory to sys.path if it's not already there
+if emulation_directory_path not in sys.path:
+    sys.path.append(emulation_directory_path)
+
+
+#%% ####### Generate ambition levels - use for both regulation and carbon pricing
+
+def ambition_generator(regions = ['EA','US', 'CN', 'IN', 'BR', 'RGS', 'RGN'], Nscens = 1):
     
+    # Number of scenarios
     Nscens = Nscens
     # List of countries
     regions = regions
     
+    # Latin hypercube sampling over 2 dimensions
     sampler = qmc.LatinHypercube(d=2)
 
+    # Generate random values for each region evenly spaced
     df = pd.DataFrame()
     for region in regions:
         values = sampler.random(n = Nscens)
@@ -44,59 +66,55 @@ def ambition_generator(regions = ['E+','US', 'CN', 'IN', 'BR', 'ROW'], Nscens = 
 
 
 
-#%% Uncertainty generator
+#%% Uncertainty generator - currently hardcoded, could be passed but need numtype e.g. integer
 
 def uncertainty_generator(Nscens = 1):
-
     # Random sampling within ranges
     Nscens = Nscens
     
-    # LHS/ Monte carlo sampling of key variables
-    MC_df = pd.DataFrame(columns = ['learning_solar', 'learning_wind', 
-                                    'lifetime_solar', 'lifetime_wind', 'grid_expansion_lead',
-                                    'south_discr', 'north_discr'])
+    # LHS sampling of key variables
+    LHS_df = pd.DataFrame(columns = ['learning_solar', 'learning_wind', 
+                                    'lifetime_solar', 'lifetime_wind', 
+                                    'lead_offshore', 'lead_onshore', 'lead_solar',
+                                    'south_discr', 'north_discr', 'elec_demand',
+                                    'gas_price', 'coal_price', 'tech_potential',
+                                    ])
 
-    # Generate uniform samples within the specified range (taken from lit)
-    # loc = lower_bound of parameter range, scale = upper-lower bounds
-    learning_rate_solar = uniform(loc= -0.303, 
-                                  scale=0.047-(-0.303)).rvs(size=(Nscens,))
-    learning_rate_wind = uniform(loc= -0.158, 
-                                  scale=0.045-(-0.158)).rvs(size=(Nscens,))
-    lifetime_solar = randint(25, 35).rvs(size = Nscens)                       
-    lifetime_wind = randint(25, 35).rvs(size = Nscens)
-    grid_expansion_duration = randint(1, 4).rvs(size = Nscens)   
+    # Define the ranges for each variable
+    # This could be taken out of the function for generalisation
+    ranges = {'learning_solar': (-0.473, -0.165), # mean = 0.319 +/- (1x standard error + noise) - Way et al. 2022
+              'learning_wind': (-0.3, -0.088), # mean 0.194 +/- (1x standard error + noise) - Way et al. 2022
+              'lifetime_solar': (20, 35), # Krey et al. 2019
+              'lifetime_wind': (20, 35), # Krey et al. 2019
+              'lead_offshore': (3, 8), # Source?
+              'lead_onshore': (1, 4), # Source?
+              'lead_solar': (1, 4), # Source?
+              'south_discr': (0.05, 0.25),
+              'north_discr': (0.05, 0.25),
+              'elec_demand': (0, 1),
+              'gas_price': (0, 1),
+              'coal_price': (0, 1),
+              'tech_potential': (0, 1),
+              }
     
-    # Discount rates
-    global_north = []
-    global_south = []
+    # Perform LHS for each variable
+    for var, (lower, upper) in ranges.items():
+        if var in ['lifetime_solar', 'lifetime_wind', 'lead_offshore', 'lead_onshore', 'lead_solar']:
+            samples = lhs(1, samples=Nscens)
+            scaled_samples = lower + np.round(samples * (upper - lower))
+        else:
+            samples = lhs(1, samples=Nscens)
+            scaled_samples = lower + samples * (upper - lower)
+        LHS_df[var] = scaled_samples.flatten()
     
-    for _ in range(Nscens):
-        # Generate 2 random discount rates
-        random_numbers = sorted([round(random.uniform(0.05, 0.25), 3) for _ in range(2)])
-        # Assign the larger to the global south, smaller to global north
-        global_south.append(random_numbers[1])
-        global_north.append(random_numbers[0])
-    
-    # Collate variables
-    MC_samples = np.vstack([learning_rate_solar,                    
-                            learning_rate_wind,                   
-                            lifetime_solar,                             
-                            lifetime_wind,
-                            grid_expansion_duration,
-                            global_south,
-                            global_north]).transpose() 
-
-    MC_samples = pd.DataFrame(MC_samples, columns= MC_df.columns)
-    
-    
-    return MC_samples
+    return LHS_df
     
     
 
 
-#%% Combine amb_generator and amb_gen_sheets
+#%% Combine amb_generator and uncertainty_generator to create scenarios, LHS STRUCTURE INCORPORATED?
 
-def scen_generator(Nscens = 5, scen_code = 'S3', regions = ['E+','US', 'CN', 'IN', 'ROW']):
+def scen_generator(Nscens, regions, scen_code = 'S3'):
     Nscens = Nscens
     regions = regions
     
